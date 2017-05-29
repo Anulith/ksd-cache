6d0fb720d9c0191ec0d0896825851c6acb23af80
Josh Potter <joshua.potter@legrand.us>
2017-05-28 23:28:35 -0400

    TreeNode and StringNode

--- ksd-cache/Program.cs
+++ ksd-cache/Program.cs
@@ -9,19 +9,42 @@ namespace ksd_cache
         public NodeNameMismatchException(string name, string nodeName)
             : base(name + "!=" + nodeName)
         {
-
         }
     }
 
+	class NodeNotFoundException : Exception
+	{
+		public NodeNotFoundException(string name) : base(name)
+		{
+		}
+	}
+
+	class NodeTypeMismatchException : Exception
+	{
+		public NodeTypeMismatchException(string msg) : base(msg)
+		{
+		}
+	}
+
+
     abstract class Node
     {
-        public Node(string name)
+		public Node(string name, Node parent)
         {
+			if (!(parent is TreeNode)) {
+				throw new NodeTypeMismatchException("[parent] Expected TreeNode");
+			}
             this.name = name;
+			this.parent = parent;
         }
 
         protected string name;
         protected Node parent;
+		protected abstract void addNode (Stack<string> names, Node n);
+		protected virtual void addNode (string name, Node n)
+		{
+			addNode (name.Split ("."), n);
+		}
 
         public abstract Node get(string key);
         public abstract string toString();
@@ -36,46 +59,153 @@ namespace ksd_cache
         public abstract void setValue(string key, byte[] value);
     }
 
-    class LeafNode : Node
-    {
-        public LeafNode(string name, string value) : base(name)
-        {
-            this.value = value;
-        }
-        
-        string value;
+	class TreeNode : Node
+	{
+		Dictionary<string, Node> children;
 
-        public override string toString()
-        {
-            return value;
-        }
+		public TreeNode(string name, Node parent) : base(name, parent)
+		{
+		}
 
-        public override void setValue(string key, string value)
-        {
-            if (key != name) throw new NodeNameMismatchException(key, name);
-            this.value = value;
-        }
+		protected override void addNode(Stack<string> names, Node n)
+		{
+			if (names.Length == 1) {
+				children [names [0]] = n;
+			} else {
+				string key = names.Pop ();
+				if(!children.ContainsKey(key)) {
+					children [key] = new TreeNode(key, this);
+				}
+				children [key].addNode (names, n);
+			}
+		}
 
-        public override void setValue(string key, Dictionary<string, string> values)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public override string toString()
+		{
+			throw new NotImplementedException();
+		}
 
-    class TreeNode : Node
-    {
-        Dictionary<string, Node> children;
+		public override int toInt()
+		{
+			throw new NotImplementedException();
+		}
 
-        public TreeNode(string name) : base(name)
-        {
-        }
+		public override double toDouble()
+		{
+			throw new NotImplementedException();
+		}
 
-        public override string getValue(string key)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public override bool toBool()
+		{
+			throw new NotImplementedException();
+		}
 
+		public override byte[] toBinary()
+		{
+			throw new NotImplementedException();
+		}
+
+		public abstract void setValue(string key, string value)
+		{
+			addNode (key, new StringNode (key, this, value));
+		}
+
+		public abstract void setValue(string key, int value)
+		{
+			addNode (key, new IntNode (key, this, value));
+		}
+
+		public abstract void setValue(string key, double value)
+		{
+			addNode (key, new DoubleNode (key, this, value));
+		}
+
+		public abstract void setValue(string key, bool value)
+		{
+			addNode (key, new BoolNode (key, this, value));
+		}
+
+		public abstract void setValue(string key, byte[] value)
+		{
+			addNode (key, new BinaryNode (key, this, value));
+		}
+	}
+
+
+	class StringNode : Node
+	{
+		string value;
+
+		public StringNode(string name, Node parent, string value) : base(name,parent)
+		{
+			this.value = value;
+		}
+
+		public override Node get(string key)
+		{
+			if (key == this.name) {
+				return this;
+			}
+
+			throw new NodeNotFoundException (key);
+		}
+
+		public override string toString()
+		{
+			return value;
+		}
+
+		public override int toInt()
+		{
+			return Int32.Parse (value);
+		}
+
+		public override double toDouble()
+		{
+			return Double.Parse (value);
+		}
+
+		public override bool toBool()
+		{
+			return bool.Parse (value);
+		}
+
+		public override byte[] toBinary()
+		{
+			return Encoding.ASCII.GetBytes(value); 
+		}
+
+		public override void setValue(string key, string value)
+		{
+			if (key != name) throw new NodeNameMismatchException(key, name);
+			this.value = value;
+		}
+
+		public override void setValue(string key, int value)
+		{
+			if (key != name) throw new NodeNameMismatchException(key, name);
+			this.parent.addNode (key, new IntNode (name, parent, value));
+		}
+
+		public override void setValue(string key, double value)
+		{
+			if (key != name) throw new NodeNameMismatchException(key, name);
+			this.parent.addNode (key, new DoubleNode (name, parent, value));
+		}
+
+		public override void setValue(string key, bool value)
+		{
+			if (key != name) throw new NodeNameMismatchException(key, name);
+			this.parent.addNode (key, new BoolNode (name, parent, value));
+		}
+
+		public override void setValue(string key, byte[] value)
+		{
+			if (key != name) throw new NodeNameMismatchException(key, name);
+			this.parent.addNode (key, new BinaryNode (name, parent, value));
+		}
+	}
+		
     class KsdCache
     {
         Node root = new TreeNode();
